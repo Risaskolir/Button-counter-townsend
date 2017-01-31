10e02bf5b895182549f9ceede41d235a4e9967c8
risaskolir <jesusislife3>
2017-01-31 14:45:42 -0500

    Added Buttons and resource strings attached to them.  Started adding framework for onClicks

--- app/src/main/java/com/example/towns/button_counter_townsend/MainActivity.java
+++ app/src/main/java/com/example/towns/button_counter_townsend/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.towns.button_counter_townsend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ascButton;
+    private Button descButton;
+    private Button clearButton;
+    private TextView countText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ascButton = (Button)findViewById(R.id.ascend_button);
+        descButton = (Button)findViewById(R.id.descend_button);
+        clearButton = (Button)findViewById(R.id.clear_button);
+        countText = (TextView)findViewById(R.id.countTextView);
+
+        
     }
 }
